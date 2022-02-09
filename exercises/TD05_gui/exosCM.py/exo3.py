f4f1bbea6e08d8df54c2de86c8a68b809240a027
Thelma GOREAU <thelma.goreau@ens.uvsq.fr>
2022-02-09 14:00:42 +0100

    OK

--- exercises/TD05_gui/exosCM.py/exo3.py
+++ exercises/TD05_gui/exosCM.py/exo3.py
@@ -4,3 +4,10 @@ canv = tk.Canvas(racine, bg = "black", height = 500, width = 500)
 canv.pack()
 bt = tk.Button(racine, text = "Redémarrer")
 bt.pack()
+
+for i in range(1, 3):
+    x = 500/3
+    canv.create_line(x*i,0,x*i,500,fill="white",)
+canv.pack()
+
+racine.mainloop()
--- exercises/TD05_gui/exosCM.py/exo5.py
+++ exercises/TD05_gui/exosCM.py/exo5.py
@@ -6,5 +6,8 @@ bt = tk.Button(racine, text = "Pause")
 bt.pack()
 
 
+canv.create_line(200,0,200,600,fill="red",)
+canv.create_line(400,0,400,600,fill="blue",)
+canv.pack()
 
 racine.mainloop()
