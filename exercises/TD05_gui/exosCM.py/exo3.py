eba4598dd06ebc22f84f9889fcc582144f473aa8
Thelma GOREAU <thelma.goreau@ens.uvsq.fr>
2022-03-08 19:00:20 +0100

    ok

--- exercises/TD05_gui/exosCM.py/exo3.py
+++ exercises/TD05_gui/exosCM.py/exo3.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
+#liste avee les objets
+l =[]
+
+#vraibles
+carré, cercle, croix = 0, 0, 0
 
 #variables
 nb_croix = 0
@@ -7,46 +12,36 @@ nb_cercles = 0
 nb_carrés = 0
 
 #fonctions
-def croix(event):
+def forme(event):
     """gestion clic partie gauche"""
-    global nb_croix, nb_carrés
+    global nb_croix, nb_carrés, nb_cercles, croix, carré, cercle, ligne1, ligne2
     X, Y = event.x, event.y
     if 0<X<500/3 :
         if nb_croix < 2:
-            canvas.create_rectangle(X-25,Y-25,X+25,Y+25, fill="white")
-            canvas.create_line(X-25,Y-25,X+25,Y+25, fill ="blue", width=5)
-            canvas.create_line(X+25,Y-25,X-25,Y+25, fill ="blue", width=5)
+            carré = canvas.create_rectangle(X-25,Y-25,X+25,Y+25, fill="white")
+            ligne1 = canvas.create_line(X-25,Y-25,X+25,Y+25, fill ="blue", width=5)
+            ligne2 = canvas.create_line(X+25,Y-25,X-25,Y+25, fill ="blue", width=5)
+            l.append(carré)
+            l.append(ligne1)
+            l.append(ligne2)
             nb_croix += 1
             nb_carrés += 1
-        else:
-            return
-
-def carré(event):
-    """gestion clic partie milieu"""
-    global nb_carrés
-    X, Y = event.x, event.y
-    if 500/3<X<1000/3:
+    elif 500/3<X<1000/3:
         if nb_carrés < 3:
-            canvas.create_rectangle(X-25,Y-25,X+25,Y+25, fill="green")
+            carré = canvas.create_rectangle(X-25,Y-25,X+25,Y+25, fill="green")
+            l.append(carré)
             nb_carrés += 1
-        else:
-            return
-
-def cercle(event):
-    """gestion clic partie droite"""
-    global nb_cercles
-    X, Y = event.x, event.y
-    if 1000/3<X<500:
+    elif 1000/3<X<500:
         if nb_cercles < 3:
-            canvas.create_oval(X-25,Y-25,X+25,Y+25, outline="red")
+            cercle = canvas.create_oval(X-25,Y-25,X+25,Y+25, outline="red")
+            l.append(cercle)
             nb_cercles += 1
-        else:
-            return
+
 
 def redemarrer():
-    éléments = canvas.find_enclosed(0, 0, 500, 500)
-    print(éléments)
-    canvas.delete(éléments)
+    for i in range(len(l)):
+        canvas.delete(int(l[i]))
+
 
 
 #création des widgets
@@ -65,11 +60,7 @@ canvas.pack()
 bouton.pack()
 
 #liaison des clic
-canvas.bind("<Button-1>", croix)
-canvas.bind("<Button-1>", carré)
-canvas.bind("<Button-1>", cercle)
-
-
+canvas.bind("<Button-1>", forme)
 
 #boucle principale
 racine.mainloop()
--- exercises/TD05_gui/exosCM.py/exo4.py
+++ exercises/TD05_gui/exosCM.py/exo4.py
@@ -1,52 +1,55 @@
 import tkinter as tk
-import math as ma
+
+#dit si le carré est fixe
+est_fixe = False
+
+#état du bouton
+n = 0  #pair=pause
+
+#fonction
+def gestion_clic_i(event):
+    global C, x0, y0, x1, y1, carré, est_fixe
+    if est_fixe:
+        return
+    if x0< event.x <x1 and y0< event.y <y1 and C >= 20:
+        x0, y0, x1, y1 = x0+5, y0+5, x1-5, y1-5
+        canvas.delete(carré)
+        carré = canvas.create_rectangle(x0, y0, x1, y1, fill="red")
+        C = C-10
+    elif (0<event.x<x0 or x1<event.x<500) and (0<event.y<y0 or y1<event.y<500) and C <= 100:
+        x0, y0, x1, y1 = x0-5, y0-5, x1+5, y1+5
+        canvas.delete(carré)
+        carré = canvas.create_rectangle(x0, y0, x1, y1, fill="red")
+        C = C+10
+        
+
+def etat_bouton():
+    global est_fixe, n
+    if n%2 == 0:
+        est_fixe = True
+        bouton.config(text="Restart")
+        n +=1
+    elif n%2 != 0:
+        est_fixe = False
+        bouton.config(text="Pause")
+        n +=1
+        
 
 #milieu du carré
 M = 250
 x0, y0, x1, y1 = M-25,M-25,M+25,M+25
 C = 50
 
-
-def gestion_clic_i(event):
-    global C
-    if x0< event.x <x1 and y0< event.y <y1 and C > 20:
-        canvas.delete(carré)
-        canvas.create_rectangle(x0+5, y0+5, x1-5, y1-5, fill="red")
-        C = C-10
-        x0, y0, x1, y1 = x0+5, y0+5, x1-5, y1-5
-    elif 0<event.x<x0 or x1<event.x<500 and 0<event.y<y0 or y1<event.y<500 and C < 100:
-        canvas.coords(carré, x0-5, y0-5, x1+5, y1+5)
-        
-
-def suspendu():
-    bouton.config(text="Restart")
-
-
-def aiguillage(condition):
-     while condition!=0: ## boucle si condition ne vaut pas 0
-          if condition==1: 
-                gestion_clic_i() ## excute "mafonction1" si condition =1
-          elif condition ==2: 
-                suspendu() ## excute "mafonction2" si condition =2
- 
- 
-     time.sleep(1) ## pause d'1 seconde avant de continuer
-     aiguillage(condition)    ## la fonction aiguillage s'appelle elle-même
-
-
-
-
 #création des widgets
 racine = tk.Tk()
 canvas = tk.Canvas(racine, bg = "white", height = 500, width = 500)
-bouton = tk.Button(racine, text = "Pause", command=suspendu)
+bouton = tk.Button(racine, text = "Pause", command=etat_bouton)
 
 #création carré
 carré = canvas.create_rectangle(x0, y0, x1, y1, fill="red")
 
 #liaison des clics
 canvas.bind("<Button-1>", gestion_clic_i)
-
 
 #positionnement des widgets
 canvas.grid()
